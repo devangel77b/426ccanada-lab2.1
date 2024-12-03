e96c870817c71c1d0c7827c093258249da514d8d
Dennis Evangelista <devangel77b@gmail.com>
2024-12-03 17:58:10 -0500

    checking data

--- canada.R
+++ canada.R
@@ -1,19 +1,21 @@
 library(ggplot2)
-raw <- read.csv("avalur.csv",header=TRUE)
+raw <- read.csv("canada.csv",header=TRUE)
 
 library(dplyr)
 data <- mutate(
  raw,
- ameas.ms2 = 2*x.m/t.s^2
+ ameas.ms2 = 2*x.m/t.s^2,
+ ahat = ameas.ms2/9.81,
+ mhat = m2.kg/(m1.kg+m2.kg+mc.kg),
  )
 
 apredicted.ms2 <- function(m2,m1,mc){
-  9.81*m2/(m2+m1+mc)
+  9.81*(m2)/(m2+m1+mc)
 }
 
 fig <- ggplot(data)+geom_point(aes(x=m1.kg,y=ameas.ms2))+
-    ylim(0,2)+xlim(0,1)+
-    geom_function(fun=apredicted.ms2,args=list(m2=0.1,mc=0.5),color='blue')+
+    ylim(0,0.2)+xlim(0,3)+
+    geom_function(fun=apredicted.ms2,args=list(m2=0.020,mc=0.5),color='blue')+
     xlab('$m_1$ (\\unit{\\kilo\\gram})')+
     ylab('$a$ (\\unit{\\meter\\per\\second\\squared})')+
     theme_bw(base_size=8)
@@ -23,6 +25,17 @@ svglite('fig5.svg',width=3,height=2,pointsize=8)
 print(fig)
 dev.off()
 
+fig2 <- ggplot(data)+geom_point(aes(x=mhat,y=ahat))+
+    ylim(0,0.02)+xlim(0,0.02)+
+    geom_abline(slope=1.2150,intercept=-0.0015,color='blue')+
+    xlab('$\\frac{m_2}{m_1+m_2+m_c}$')+
+    ylab('$\\frac{a}{g}$')+
+    theme_bw(base_size=8)
+svglite('fig6.svg',width=3,height=2,pointsize=8)
+print(fig2)
+dev.off()
+
+
 library(xtable)
 results <- summarize(
 	mean.t = mean(t.s),
@@ -31,8 +44,7 @@ results <- summarize(
 	sd.a = sd(ameas.ms2),
 	group_by(data,m1.kg)
 	)
-print(xtable(results),include.rownames=FALSE,file='table1raw.tex')
+print(xtable(results,digits=4),include.rownames=FALSE,file='table1raw.tex')
 
-# for fun try a nonlinear fit of the data to find g and mc? 
-foo <- nls(ameas.ms2~g*m2.kg/(m1.kg+m2.kg+mc),data=data,start=c(g=9.81,mc=0.5))
-summary(foo)
+fit <- lm(ahat~mhat,data=data)
+summary(fit)
